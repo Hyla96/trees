e732560b0ce986ed09fc93067c84805f6bc7926f
Gabriel <gabriel.costache@mobilityhouse.com>
2025-08-21 23:18:22 +0200

    feat: insertion function

--- binary_search_tree/src/binary_search_tree.rs
+++ binary_search_tree/src/binary_search_tree.rs
@@ -19,11 +19,13 @@ impl BinarySearchTree {
         };
     }
 
-    pub fn insert(&mut self, node: Box<Node>) {
-        if self.root.is_none() {
-            self.root = Some(node);
-            return;
+    pub fn insert(&mut self, node: Box<Node>) -> bool {
+        if let Some(ref mut root) = self.root {
+            return insert_node(root, node);
         }
+
+        self.root = Some(node);
+        return false;
     }
 
     pub fn delete(&mut self, key: i64) {}
@@ -43,6 +45,26 @@ impl BinarySearchTree {
             print_node(root)
         }
     }
+}
+
+fn insert_node(root: &mut Box<Node>, node: Box<Node>) -> bool {
+    if root.key == node.key {
+        return true;
+    }
+
+    if node.key > root.key {
+        if let Some(ref mut right) = root.right {
+            return insert_node(right, node);
+        }
+        root.right = Some(node);
+        return false;
+    }
+
+    if let Some(ref mut left) = root.left {
+        return insert_node(left, node);
+    }
+    root.left = Some(node);
+    return false;
 }
 
 fn print_node(node: &Box<Node>) {
