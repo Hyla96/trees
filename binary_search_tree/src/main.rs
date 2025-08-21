b52644c74f83fa67afdfba8a182d7b5abf48eb4c
Gabriel <gabriel.costache@mobilityhouse.com>
2025-08-21 23:29:21 +0200

    fix: easier node init

--- binary_search_tree/src/main.rs
+++ binary_search_tree/src/main.rs
@@ -1,61 +1,25 @@
 pub mod binary_search_tree;
 
-fn main() {
-    let root = binary_search_tree::Node {
-        key: 10,
-        value: "Root Node".to_string(),
-        left: None,
-        right: None,
-    };
+const NODES: [(i64, &str); 6] = [
+    (10, "Root Node"),
+    (12, "Node 1"),
+    (19, "Node 2"),
+    (6, "Node 3"),
+    (2, "Node 4"),
+    (11, "Node 5"),
+];
 
+fn main() {
     let mut bst = binary_search_tree::BinarySearchTree::new();
 
-    bst.insert(Box::new(root));
-
-    let node_1 = binary_search_tree::Node {
-        key: 12,
-        value: "Node 1".to_string(),
-        left: None,
-        right: None,
-    };
-
-    bst.insert(Box::new(node_1));
-
-    let node_2 = binary_search_tree::Node {
-        key: 19,
-        value: "Node 2".to_string(),
-        left: None,
-        right: None,
-    };
-
-    bst.insert(Box::new(node_2));
-
-    let node_3 = binary_search_tree::Node {
-        key: 6,
-        value: "Node 3".to_string(),
-        left: None,
-        right: None,
-    };
-
-    bst.insert(Box::new(node_3));
-
-    let node_4 = binary_search_tree::Node {
-        key: 2,
-        value: "Node 4".to_string(),
-        left: None,
-        right: None,
-    };
-
-    bst.insert(Box::new(node_4));
-
-    let node_5 = binary_search_tree::Node {
-        key: 11,
-        value: "Node 5".to_string(),
-        left: None,
-        right: None,
-    };
-
-    bst.insert(Box::new(node_5));
+    for (key, value) in NODES.iter() {
+        bst.insert(Box::new(binary_search_tree::Node {
+            key: *key,
+            left: None,
+            right: None,
+            value: value.to_string(),
+        }));
+    }
 
     bst.print_tree();
 }
